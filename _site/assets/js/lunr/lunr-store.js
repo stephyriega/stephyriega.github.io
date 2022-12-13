d8a38a91614c96e409a5b5d2d8b146fb72283eb0
Stephy  Riega <73911661+stephyriega@users.noreply.github.com>
2022-12-13 15:08:08 -0500

    drop _site

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Discrete Choice Models - Matlab",
-        "excerpt":"Discrete choice models in MatlabFork my repo here.Models available: Logit Exploded Logit Logit with Random Coefficients Exploded Logit with Random CoefficientsDiscrete choice models are becoming trendier as more data becomes available in multiple areas like health, education, credit markets, and many others. The key idea of them is to model...","categories": [],
-        "tags": ["logit","discrete choice model","behavioral model"],
-        "url": "http://localhost:4000/dcm/",
-        "teaser":null}]
